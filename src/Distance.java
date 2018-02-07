3e05b8219e886477d5a28f38e8cd9f28812b22a4
Mike Baldwin <Michael.Baldwin3@rockets.utoledo.edu>
2018-02-07 09:49:11 -0500

    More formatting changes to adhere to the required coding style, and added the blank struts back in.

--- src/Distance.java
+++ src/Distance.java
@@ -6,24 +6,24 @@ public class Distance {
 
 public static void main(String[] args) {
     JPanel panel = new JPanel();
-    JTextField x1Var = new JTextField(5);
-    JTextField y1Var = new JTextField(5);
-    JTextField x2Var = new JTextField(5);
-    JTextField y2Var = new JTextField(5);
+    JTextField x1Var = new JTextField(3);
+    JTextField y1Var = new JTextField(3);
+    JTextField x2Var = new JTextField(3);
+    JTextField y2Var = new JTextField(3);
     double x1, x2, y1, y2, distance;
     int confirmCode;
 
-    panel.add(new JLabel("x1: "));
-    panel.add(x1Var);
-    panel.add(new JLabel("y1: "));
-    panel.add(y1Var);
-    panel.add(new JLabel("x2: "));
-    panel.add(x2Var);
-    panel.add(new JLabel("y2: "));
-    panel.add(y2Var);
+    panel.add(new JLabel("x1: ")); panel.add(x1Var);  panel.add(Box.createHorizontalStrut(15));
+    panel.add(new JLabel("y1: ")); panel.add(y1Var);  panel.add(Box.createVerticalStrut(15));
+    panel.add(new JLabel("x2: ")); panel.add(x2Var);  panel.add(Box.createHorizontalStrut(15));
+    panel.add(new JLabel("y2: ")); panel.add(y2Var);
 
-    confirmCode = JOptionPane.showConfirmDialog(null, panel,
-            "Distance Calculator", JOptionPane.OK_CANCEL_OPTION);
+    confirmCode = JOptionPane.showConfirmDialog(
+            null,
+            panel,
+            "Distance Calculator",
+            JOptionPane.DEFAULT_OPTION);
+
     if (confirmCode == JOptionPane.OK_OPTION) {
         x1 = Double.parseDouble(x1Var.getText());
         y1 = Double.parseDouble(y1Var.getText());
